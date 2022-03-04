27ed3ea1afc54eeaeb73bd2bf11aa27141e1c607
Justin Addams <14937219+UnicodeTreason@users.noreply.github.com>
2022-03-04 15:45:52 +0800

    Add DNS

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ provider "vultr" {
   # export VULTR_API_KEY="Your Vultr API Key"
 }
 
+# Configure VM
 resource "vultr_instance" "srv02" {
   region    = var.region_sydney
   os_id     = var.os_centos_9
@@ -19,4 +20,12 @@ resource "vultr_instance" "srv02" {
   hostname  = "srv02"
   label     = "Zabbix Host"
   tag       = "terraform"
+}
+
+# Configure DNS
+resource "vultr_dns_record" "srv02unicodetreasoncom" {
+    domain = "unicodetreason.com"
+    name = "srv02"
+    data = vultr_instance.srv02.main_ip
+    type = "A"
 }
